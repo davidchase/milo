6a100b8de76dad3d9eee2ae5dd55e0fc125a5bc1
David Annopolsky <dannopolsky@freepeople.com>
2014-08-24 10:55:46 -0400

    added structure

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 var internals = {};
 internals.classRegex = function(className) {
     return new RegExp('(^|\\s+)' + className + '(\\s+|$)');
@@ -17,6 +17,41 @@ internals.extend = function(obj) {
         }
     });
     return obj;
+};
+internals.now = Date.now || function() {
+    return new Date().getTime();
+};
+
+internals.throttle = function(func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    options || (options = {});
+    var later = function() {
+        previous = options.leading === false ? 0 : internals.now();
+        timeout = null;
+        result = func.apply(context, args);
+        context = args = null;
+    };
+    return function() {
+        var now = internals.now();
+        if (!previous && options.leading === false) {
+            previous = now;
+        }
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0) {
+            clearTimeout(timeout);
+            timeout = null;
+            previous = now;
+            result = func.apply(context, args);
+            context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result;
+    };
 };
 var Bloccs = function() {
     this.config = {};
