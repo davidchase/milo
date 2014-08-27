74837be66ca46267c05f5ef9a27f8708102438df
David Annopolsky <dannopolsky@freepeople.com>
2014-08-27 11:35:17 -0400

    updated comments and creditsL Bill Murray

--- milo.js
+++ milo.js
@@ -19,12 +19,12 @@ MiloGridProto._calcTopPosition = function(index) {
     return index >= this.gridColumns ? this.topOffset[index - this.gridColumns] : 0;
 };
 
+// to calculate the columns
+// get the current available width
+// then divide it by gridItem width + gridItem margin
+// we are using Math.floor to get the most possible
+// amount of columns to fit into the area allocated
 MiloGridProto._calcGridColumns = function() {
-    // to calculate the columns
-    // get the current available width
-    // then divide it by gridItem width + gridItem margin
-    // we are using Math.floor to get the lowest possible
-    // amount of columns to fit into the area allocated
     this.containerWidth = this.containerEl.clientWidth;
     this.gridColumns = Math.floor((this.containerWidth / (this.gridItemWidth + this.gridItemMargin)));
 };
