fd673bedddf241f60004ca38e06430647c677997
David Annopolsky <dannopolsky@freepeople.com>
2015-08-06 20:49:07 -0400

    rebundled example

--- example/bundle.js
+++ example/bundle.js
@@ -1,5 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
+var Milo = require('../milo');
+
+var milo = new Milo({
+    container: '.grid'
+});
+
+window.addEventListener('load', milo.buildGrid);
+},{"../milo":3}],2:[function(require,module,exports){
+'use strict';
 module.exports = function debounce(fn, threshold, isAsap) {
     var timeout, result;
 
@@ -26,7 +35,7 @@ module.exports = function debounce(fn, threshold, isAsap) {
     };
     return debounced;
 };
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 'use strict';
 
 var debounce = require('./lib/debounce');
@@ -102,4 +111,4 @@ var Milo = function(options) {
 };
 
 module.exports = Milo;
-},{"./lib/debounce":1}]},{},[2]);
+},{"./lib/debounce":2}]},{},[1]);
